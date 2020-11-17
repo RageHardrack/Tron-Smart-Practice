8f2ce052d392d9fc3c0108a9acdb5db98d476e02
Dan Colmenares <dacolmenares93@gmail.com>
2020-11-16 19:16:34 -0500

    Haciendo retoques

--- src/components/How.vue
+++ src/components/How.vue
@@ -2,49 +2,75 @@
   <v-container>
     <h1>How To Make Deposit?</h1>
 
-    <v-card>
-      <v-card-title>
-        Make Deposit
-      </v-card-title>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-container class="pa-5">
+            <v-card-title>
+              <h2>
+                Make Deposit
+              </h2>
+            </v-card-title>
 
-      <v-card-subtitle>
-        GET TRON (TRX)
-      </v-card-subtitle>
+            <v-row>
+              <v-card-subtitle>
+                GET TRON (TRX)
+              </v-card-subtitle>
 
-      <v-card-action name="GET">
-        <v-btn color="light">HOW TO GET TRX</v-btn>
-      </v-card-action>
+              <v-card-action name="GET">
+                <v-btn color="light">HOW TO GET TRX</v-btn>
+              </v-card-action>
+            </v-row>
 
-      <v-card-text>
-        <h2>Connect With Your Wallet App</h2>
-      </v-card-text>
+            <v-row justify="center">
+              <v-card-text class="text-center">
+                <h2>Connect With Your Wallet App</h2>
+              </v-card-text>
 
-      <v-card-action name="Wallet">
-        <v-btn color="light">Connect Wallet</v-btn>
-      </v-card-action>
-    </v-card>
+              <v-card-action name="Wallet">
+                <v-btn color="light">Connect Wallet</v-btn>
+              </v-card-action>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
 
-    <v-card>
-      <v-card-title>
-        <h2>Request Withdraw</h2>
-      </v-card-title>
+      <v-col>
+        <v-card>
+          <v-container class="pa-5">
+            <v-card-title>
+              <h2>Request Withdraw</h2>
+            </v-card-title>
 
-      <v-card-subtitle>
-        Your Payout Will Come Instantly
-      </v-card-subtitle>
+            <v-card-text>
+              <v-list>
+                <v-list-item>
+                  <v-card-subtitle>
+                    Your Payout Will Come Instantly
+                  </v-card-subtitle>
+                </v-list-item>
 
-      <v-card-subtitle>
-        Request Withdraw From The Same Wallet You Deposited
-      </v-card-subtitle>
+                <v-list-item>
+                  <v-card-subtitle>
+                    Request Withdraw From The Same Wallet You Deposited
+                  </v-card-subtitle>
+                </v-list-item>
+              </v-list>
+            </v-card-text>
 
-      <v-card-text>
-        <h2>Connect With Your Wallet App</h2>
-      </v-card-text>
+            <v-row justify="center">
+              <v-card-text class="text-center">
+                <h2>Connect With Your Wallet App</h2>
+              </v-card-text>
 
-      <v-card-action name="Wallet">
-        <v-btn color="light">Connect Wallet</v-btn>
-      </v-card-action>
-    </v-card>
+              <v-card-action name="Wallet">
+                <v-btn color="light">Connect Wallet</v-btn>
+              </v-card-action>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
--- src/components/InvestConditions.vue
+++ src/components/InvestConditions.vue
@@ -1,54 +1,63 @@
 <template>
   <v-container>
-    <h1>Investment Conditions</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-      perspiciatis officia nisi omnis et nesciunt dolor aut nam. Delectus
-      obcaecati perferendis facilis error earum necessitatibus omnis at maiores,
-      adipisci autem.
-    </p>
-
-    <v-card>
-      <v-card-title>2.12%</v-card-title>
-      <v-card-subtitle>Current Daily</v-card-subtitle>
-
-      <v-card-text>Total Max Profit 150% (Profit 0% Per Hour)</v-card-text>
-    </v-card>
-
-    <v-card>
-      <v-row>
-        <v-col>
-          <h3>Personal Hold-Bonus:</h3>
-          <h2>+0.05% Per 25 Hours</h2>
-          <h3>Max 1%</h3>
-          <h4>Without Withdrawal</h4>
-        </v-col>
-
-        <v-col>
-          <h3>Total Profit:</h3>
-          <h2>150%</h2>
-          <h3>Deposit Included</h3>
-        </v-col>
-
-        <v-col>
-          <h3>Minimum Deposit:</h3>
-          <h2>100 TRX</h2>
-        </v-col>
-
-        <v-col>
-          <h3>Maximum Deposit:</h3>
-          <h2>Unlimited</h2>
-        </v-col>
-      </v-row>
-    </v-card>
+    <v-row>
+      <h1>Investment Conditions</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+        perspiciatis officia nisi omnis et nesciunt dolor aut nam. Delectus
+        obcaecati perferendis facilis error earum necessitatibus omnis at
+        maiores, adipisci autem.
+      </p>
+    </v-row>
 
     <v-row>
-      <v-col>
-        <h2>Earnings in realtime</h2>
+      <v-col cols="2">
+        <v-card>
+          <v-card-title>2.12%</v-card-title>
+          <v-card-subtitle>Current Daily</v-card-subtitle>
+
+          <v-card-text>Total Max Profit 150% (Profit 0% Per Hour)</v-card-text>
+        </v-card>
       </v-col>
 
-      <v-col>
-        <h2>Withdraw anytime</h2>
+      <v-col cols="10">
+        <v-card>
+          <v-container class="pa-5">
+            <v-row>
+              <v-col>
+                <h3>Personal Hold-Bonus:</h3>
+                <h2>+0.05% Per 25 Hours</h2>
+                <h3>Max 1%</h3>
+                <h4>Without Withdrawal</h4>
+              </v-col>
+
+              <v-col>
+                <h3>Total Profit:</h3>
+                <h2>150%</h2>
+                <h3>Deposit Included</h3>
+              </v-col>
+
+              <v-col>
+                <h3>Minimum Deposit:</h3>
+                <h2>100 TRX</h2>
+              </v-col>
+
+              <v-col>
+                <h3>Maximum Deposit:</h3>
+                <h2>Unlimited</h2>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+        <v-row>
+          <v-col>
+            <h2>Earnings in realtime</h2>
+          </v-col>
+
+          <v-col>
+            <h2>Withdraw anytime</h2>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
