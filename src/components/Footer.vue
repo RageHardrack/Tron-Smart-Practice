1f60d0da7c761541fe76baded8df7096aacb0bd6
Dan Colmenares <dacolmenares93@gmail.com>
2020-11-19 19:49:34 -0500

    Actualizando el footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,26 @@
-.<template>
-  <v-card>
-    <v-card-title>2020 Tron Smart</v-card-title>
+<template>
+  <v-footer dark padless>
+    <v-card flat tile class="primary white--text text-center">
+      <v-card-title>Contact Us</v-card-title>
 
-    <v-container>
-      <v-card-title>Smart-Contract Address:</v-card-title>
-      <v-card-subtitle>aioc0ianwoncmapknaoin</v-card-subtitle>
-    </v-container>
+      <v-container>
+        <v-card-title>Smart-Contract Address:</v-card-title>
+        <v-card-subtitle>aioc0ianwoncmapknaoin</v-card-subtitle>
+      </v-container>
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <v-card-title>Contact Us</v-card-title>
-  </v-card>
+      <v-card-text class="white--text">2020 Tron Smart</v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.v-card {
+  width: 100%;
+}
+</style>
